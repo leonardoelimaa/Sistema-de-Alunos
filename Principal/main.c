c2e4d253ad58d7ef0aa2db341df8bef0a2f787d6
Leonardo Lima <leonardoelima2601@gmail.com>
2025-04-02 16:39:20 -0300

    Update main.c

--- Principal/main.c
+++ Principal/main.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
-
-char acessos_professor_cadastrados [4][2][30] = {
-    {"leonardoelima", "senhaleonardo"},
-    {"pedrobrito", "senhapedro"},
-    {"danilopimentel", "senhadanilo"},
-    {"joaopaulo", "senhajoao"}
-};
-char usuario_professor[30];
-char senha_professor[30];
+#include <stdlib.h>
 
 
 int tela_de_inicio();
 int login_professor();
+int login_professor();
+int menu_iniciar();
 
 
 int main () {
+    system("cls");
+    
     int retorno_acesso = tela_de_inicio(); // Atualmente retornando 2
-    int retorno_login; // Atualmente retornando 2
+    int retorno_login; // Atualmente retornando 
+    int retorno_menu; 
 
     switch (retorno_acesso) {
         case 1: 
@@ -34,7 +31,25 @@ int main () {
             printf("Pagina de aluno ainda nao criada!");
             break;
         case 2:           
-            printf("Codigo ta funcionando");
+            retorno_menu = menu_iniciar();
+            break;
+    }  
+
+    switch (retorno_menu) {
+        case 1:
+            printf("Buscar estudantes ainda nao criado");
+            break;
+        case 2:           
+            printf("Cadastro de estudantes ainda nao criado!");
+            break;
+        case 3:           
+            printf("Cadastro de notas ainda nao criado!");
+            break;
+        case 4:           
+            printf("Opcao de observacoes ainda nao criado!");
+            break;
+        case 5:           
+            printf("Opcao de relatorios ainda nao criado!");
             break;
     }  
 }
