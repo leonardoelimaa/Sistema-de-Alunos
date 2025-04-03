07413e3ff4ccafba551ebecb97e1a64a0aa2e468
dalladea <lucassdalladea@gmail.com>
2025-04-03 13:32:45 -0300

    Update and rename main (1).c to classificação.c

---
+++ classificação.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+
+char usuario_aluno
+float media;
+
+ if (media >= 6){
+printf("O aluno %s, com %.2f de média, foi aprovado!", usuario_aluno, media);
+ }
+else{
+printf("O aluno %s, com %.2f de média, foi reprovado.", usuario_aluno, media);
+}
+
+}
