0a72645f6cefda6b4d43fafe8bffe682bcd73cfd
Leonardo Lima <leonardoelima2601@gmail.com>
2025-04-10 01:23:32 -0300

    Create sistema.h

---
+++ Bblioteca/sistema.h
@@ -0,0 +1,17 @@
+#ifndef SISTEMA_H
+#define SISTEMA_H
+
+int tela_de_inicio();
+int login_professor();
+int menu_iniciar();
+void media(struct provas aluno);
+int classificacao();
+void cadastrarAluno();
+void listarAlunos();
+int buscarPorMatricula(int matricula);
+void buscarAluno();
+void editarAluno();
+void removerAluno();
+void menu();
+
+#endif
