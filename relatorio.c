eaf6ebe052da5a075a0aa6f8d3b84a1b7c7a4795
dalladea <lucassdalladea@gmail.com>
2025-04-13 22:24:03 -0300

    Update relatorio.c

--- relatorio.c
+++ relatorio.c
@@ -1,36 +1,37 @@
 #include <stdio.h>
 
-int relatorio(){
+int main() {
 
-char usuario_professor[40], nome_aluno[100][100];
-int quant_de_provas, quant_de_alunos, i;
-float media[100], n1[100], n2[100], n3[100];
+	char usuario_professor[40], nome_aluno[100][100];
+	int quant_de_provas, quant_de_alunos, i;
+	float media[100], n1[100], n2[100], n3[100];
 
-printf("Número de alunos: ");
-scanf("%d", &quant_de_alunos);
+	printf("Número de alunos: ");
+	scanf("%d", &quant_de_alunos);
 
-for (i=0; i < quant_de_alunos; i++){
-printf("\n\nNome do %dº aluno:\n", i+1);
-scanf("%s", nome_aluno[i]);
-printf("Nota da prova 1 do %dº aluno:\n", i+1);
-scanf("%f", &n1[i]);
-printf("Nota da prova 2 do %dº aluno:\n", i+1);
-scanf("%f", &n2[i]);
-printf("Nota da prova 3 do %dº aluno:\n", i+1);
-scanf("%f", &n3[i]);
-media[i] = (n1[i] + n2[i] + n3[i]) / 3;
-}
+	for (i=0; i < quant_de_alunos; i++) {
+		printf("\n\nNome do %dº aluno:\n", i+1);
+		scanf(" %[^\n]", nome_aluno[i]);
+		printf("Nota da prova 1 do %dº aluno:\n", i+1);
+		scanf("%f", &n1[i]);
+		printf("Nota da prova 2 do %dº aluno:\n", i+1);
+		scanf("%f", &n2[i]);
+		printf("Nota da prova 3 do %dº aluno:\n", i+1);
+		scanf("%f", &n3[i]);
+		media[i] = (n1[i] + n2[i] + n3[i]) / 3;
+	}
 
-printf("\n----------Relatório de Desempenho----------\n", usuario_professor);
-printf("\nNome do aluno\t Nota 1\t Nota 2\t Nota 3\t Média\t Aprovação\n");
+	printf("\n----------Relatório do professor %s----------\n", usuario_professor);
+	printf("\nNome do aluno\t Nota 1\t Nota 2\t Nota 3\t Média\t Aprovação\n");
+	printf("----------------------------------------------------------\n");
 
-for(i=0; i < quant_de_alunos; i++){
-if(media[i] >= 6){
-printf("%s\t %.2f\t %.2f\t %.2f\t %.2f\t APROVADO\n",nome_aluno[i], n1[i], n2[i], n3[i], media[i]);
-}
-else{
-printf("%s\t %.2f\t %.2f\t %.2f\t %.2f\t REPROVADO\n",nome_aluno[i], n1[i], n2[i], n3[i], media[i]);
-}
-}
+	for(i=0; i < quant_de_alunos; i++) {
+		if(media[i] >= 6) {
+			printf("%s\t %.2f\t %.2f\t %.2f\t %.2f\t APROVADO\n",nome_aluno[i], n1[i], n2[i], n3[i], media[i]);
+		}
+		else {
+			printf("%s\t %.2f\t %.2f\t %.2f\t %.2f\t REPROVADO\n",nome_aluno[i], n1[i], n2[i], n3[i], media[i]);
+		}
+	}
 
 }
