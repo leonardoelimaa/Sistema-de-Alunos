1d2f660c5169e0f542b5a1c26b5ee61e47fe9b02
leonardoelima <leonardoelima2601@gmail.com>
2025-06-03 18:49:45 -0300

    Ajuste de formatacao dos comentarios na biblioteca .h

--- Biblioteca/sistema.h
+++ Biblioteca/sistema.h
@@ -2,19 +2,19 @@
 #define SISTEMA_H
 
 // ==== BIBLIOTECAS ====
-#include <stdio.h>         //biblioteca padrão de entrada e saída
-#include <stdlib.h>       //biblioteca de funções de uso comum
-#include <string.h>      //biblioteca de strings
-#include <ctype.h>      //biblioteca para manipular caracteres
-#include <time.h>      //biblioteca para usar funções relacionadas ao tempo
+#include <stdio.h>         // Biblioteca padrão de entrada e saída
+#include <stdlib.h>       // Biblioteca de funções de uso comum
+#include <string.h>      // Biblioteca de strings
+#include <ctype.h>      // Biblioteca para manipular caracteres
+#include <time.h>      // Biblioteca para usar funções relacionadas ao tempo
 
 // ==== CONSTANTES ====
 
-#define MAX_ALUNOS 100   //constante do número de alunos
-#define MAX_NOME 100    //constante do número máximo de caracteres de um nome
-#define MAX_CURSO 50   //constante do número máximo de caracteres do nome do curso
-#define MAX_OBS 200   //costante que define o tamanho máximo das observações
-#define MAX_NOTAS 3  //constante que define o número de notas a serem cadastradas
+#define MAX_ALUNOS 100   // Constante do número de alunos
+#define MAX_NOME 100    // Constante do número máximo de caracteres de um nome
+#define MAX_CURSO 50   // Constante do número máximo de caracteres do nome do curso
+#define MAX_OBS 200   // Costante que define o tamanho máximo das observações
+#define MAX_NOTAS 3  // Constante que define o número de notas a serem cadastradas
 
 // ==== STRUCT ====
 
@@ -30,19 +30,19 @@ typedef struct {
 
 // ==== PROTOTIPOS DAS FUNCOES ====
 
-int tela_de_inicio();
+int  tela_de_inicio();
 void limpa_tela();
-int login_professor();
-int menu_iniciar();
+int  login_professor();
+int  menu_iniciar();
 void media(int i);
 void classificacao(int i);
 void classificacao();
 void limparBuffer();
-int matriculaExiste(int matricula);
-int gerarMatricula();
+int  matriculaExiste(int matricula);
+int  gerarMatricula();
 void cadastrarAluno();
 void listarAlunos();
-int buscarPorMatricula(int matricula);
+int  buscarPorMatricula(int matricula);
 void buscarAluno();
 void editarAluno();
 void removerAluno();
